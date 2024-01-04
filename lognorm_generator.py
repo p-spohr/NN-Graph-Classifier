6b22bb4f015c5c03cf3af819da7afaf864d977f9
Patrick <spohrpatrick@gmail.com>
2024-01-04 18:57:50 +0100

    combine scripts of generators on one file

--- lognorm_generator.py
+++ lognorm_generator.py
@@ -5,11 +5,10 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import random as random
 import os
 import time
 
-from lorem_ipsum_prep import lorumipsum as text
+from lorem_ipsum_prep import lorumipsum as LORUMIPSUM
 
 # %%
 
@@ -17,116 +16,260 @@ from lorem_ipsum_prep import lorumipsum as text
 
 # measure script runtime
 start_time = time.time()
-
 # use lorem ipsum to avoid language bias
-words = np.array(text)
-rng = np.random.default_rng()
+LORUMIPSUM = np.array(LORUMIPSUM)
+# init random generator
+RNG = np.random.default_rng(12345)
 
-rand_rgb = [] # randomized rgb
+# RGB color values for bins, lines, texts
 rgb_bin = [] # bin color of histogram
 rgb_line = [] # color of lines
 rgb_face = [] # face color of figure
 rgb_ax = [] # inside color of figure
+rgb_xlabel = [] # color of text x label
+rgb_ylabel = [] # color of text y label
+rgb_title = [] # color of text title
 
-rgb_white = (1,1,1)
-rgb_black = (0,0,0)
-graph_amount = 50
+RGB_WHITE = (1,1,1)
+RGB_BLACK = (0,0,0)
+GRAPHS = 20 # 2000 graphs: 305.011 seconds
 
-# select save directory and format
-save_path = "C:\\Users\\pat_h\\htw_berlin_datasets\\dist_datasets\\lognorm"
-save_format = 'jpg'
-save_dpi = 72
+# select save directory
+SAVE_PATH = "C:\\Users\\pat_h\\htw_berlin_datasets\\dist_datasets\\lognorm"
+# picture format
+SAVE_DPI = 72
+SAVE_FORMAT = 'jpg'
 
-for graph in range(graph_amount):
+for graph in range(GRAPHS):
     
-    # reset color lists for each iteration
-    rand_rgb = []
+     # reset color RGB values for each iteration
     rgb_bin = []
     rgb_line = []
     rgb_face = []
     rgb_ax = []
+    rgb_xlabel = []
+    rgb_ylabel = []
+    rgb_title = []
 
     fig, ax = plt.subplots()
 
     # randomize parameters
-    mu = rng.integers(-3.5,3.5) * rng.random()  # mean of distribution
-    sigma = rng.integers(1,2.5) * rng.random()  # standard deviation of distribution
-    s = rng.lognormal(mean=mu, sigma=sigma, size=rng.integers(50,2000))
-
-    num_bins = rng.integers(15,100)
+    mu = RNG.integers(-3.5,3.5) * RNG.random()  # mean of distribution
+    sigma = RNG.integers(1,2.5) * RNG.random()  # standard deviation of distribution
+    s = RNG.lognormal(mean=mu, sigma=sigma, size=RNG.integers(50,2000))
+    num_bins = RNG.integers(15,100)
 
     # randomize color of bins
     for i in range(3):
-
-        color = round(random.random(), 1)
+        color = round(RNG.random(), 1)
         rgb_bin.append(color)
 
-    # add alpha 0 to hide bins 50% of the time
-    rand_int = random.random()
-    if rand_int > 0.5:
+     # add alpha 0 to hide bins 50% of the time (only function line is visible)
+    if RNG.random() > 0.5:
         rgb_bin.append(0)
 
     # the histogram of the data
-    count, bins, ignored = plt.hist(s, num_bins, density=True, color=rgb_bin) # align='mid'
+    count, bins, ignored = plt.hist(s, num_bins, density=True, color=rgb_bin)
 
     # add a 'best fit' line
     x = np.linspace(min(bins), max(bins), 1000)
     y = np.exp(-(np.log(x) - mu)**2 / (2 * sigma**2)) / (x * sigma * np.sqrt(2 * np.pi))
 
+    # color of line
     for i in range(3):
-
-        color = round(random.random(), 1)
+        color = round(RNG.random(), 1)
         rgb_line.append(color)
 
-    if rng.random() > 0.5:
+    # solid or dashed line
+    if RNG.random() > 0.5:
         line_style = '-'
     else:
         line_style = '--'
 
-    ax.plot(x, y, line_style, color=rgb_line, linewidth=rng.random()*3)
+    ax.plot(x, y, line_style, color=rgb_line, linewidth=RNG.random()*3)
 
     # fig face color
     for i in range(3):
-
-        color = round(random.random(), 1)
+        color = round(RNG.random(), 1)
         rgb_face.append(color)
-
-    if rng.random() > 0.6: # create more white graphs
-        rgb_face = rgb_white
+    if rgb_face == RGB_BLACK:
+        rgb_face == RGB_WHITE
+    if RNG.random() > 0.6: # create more white graphs
+        rgb_face = RGB_WHITE
 
     fig.set_facecolor(rgb_face)
 
     # ax face color
     for i in range(3):
-
-        color = round(random.random(), 1)
+        color = round(RNG.random(), 1)
         rgb_ax.append(color)
-    
-    if rng.random() > 0.6:
-        rgb_ax = rgb_white
+    if RNG.random() > 0.6:
+        rgb_ax = RGB_WHITE
 
     ax.set_facecolor(rgb_ax)
+    
+    # x label rgb
+    for i in range(3):
+        color = round(RNG.random(), 1)
+        rgb_xlabel.append(color)
+    # y label rgb
+    for i in range(3):
+        color = round(RNG.random(), 1)
+        rgb_ylabel.append(color)
+    # title rgb
+    for i in range(3):
+        color = round(RNG.random(), 1)
+        rgb_title.append(color)
 
     # set lorum ipsum labels
-    ax.set_ylabel(' '.join(np.random.choice(text, size = rng.integers(2,5), replace=False)))
-    ax.set_xlabel(' '.join(np.random.choice(text, size = rng.integers(2,5), replace=False)))
-    ax.set_title(' '.join(np.random.choice(text, size = rng.integers(2,5), replace=False)))
+    ax.set_ylabel(' '.join(np.random.choice(LORUMIPSUM, size = RNG.integers(2,5), replace=False)), color=rgb_ylabel, fontsize=max(10, RNG.integers(20,30)*RNG.random()))
+    ax.set_xlabel(' '.join(np.random.choice(LORUMIPSUM, size = RNG.integers(2,5), replace=False)), color=rgb_xlabel, fontsize=max(10, RNG.integers(20,30)*RNG.random()))
+    ax.set_title(' '.join(np.random.choice(LORUMIPSUM, size = RNG.integers(2,5), replace=False)), color=rgb_title, fontsize=max(10, RNG.integers(20,30)*RNG.random()))
 
     # add grid and adjust x limit 50% of the time
-    if rng.random() > 0.5:
-        plt.grid(color='k', linestyle='-', linewidth=rng.random())
+    if RNG.random() > 0.5:
+        plt.grid(color='k', linestyle='-', linewidth=RNG.random())
 
     # adjust x limit 50% of the time
-    if rng.random() > 0.5:
+    if RNG.random() > 0.5:
         plt.xlim(0)
 
     # Tweak spacing to prevent clipping of ylabel
     fig.tight_layout()
 
-    fig.savefig(os.path.join(save_path, f'lognorm_{graph}.{save_format}'), dpi=72)
+    # save graph to folder path
+    fig.savefig(os.path.join(SAVE_PATH, f'norm_{graph}.{SAVE_FORMAT}'), dpi=SAVE_DPI)
     plt.close(fig) # save memory usage
 
 end_time = time.time()
 print(f'Runtime: {round(end_time - start_time, 3)} seconds')
+
+# %%
+
+def lognorm_graph_generator(num_graphs, save_path, save_dpi, save_format, lorumipsum):
+    ### generates and saves highly randomized graphs of the log-normal distribution ###
+
+    # measure script runtime
+    start_time = time.time()
+    # use lorem ipsum to avoid language bias
+    lorumipsum = np.array(lorumipsum)
+    # init random generator
+    RNG = np.random.default_rng(12345)
+
+    # RGB color values for bins, lines, texts
+    rgb_bin = [] # bin color of histogram
+    rgb_line = [] # color of lines
+    rgb_face = [] # face color of figure
+    rgb_ax = [] # inside color of figure
+    rgb_xlabel = [] # color of text x label
+    rgb_ylabel = [] # color of text y label
+    rgb_title = [] # color of text title
+
+    RGB_WHITE = (1,1,1)
+    RGB_BLACK = (0,0,0)
+
+    for graph in range(num_graphs):
+        
+        # reset color RGB values for each iteration
+        rgb_bin = []
+        rgb_line = []
+        rgb_face = []
+        rgb_ax = []
+        rgb_xlabel = []
+        rgb_ylabel = []
+        rgb_title = []
+
+        fig, ax = plt.subplots()
+
+        # randomize parameters
+        mu = RNG.integers(-3.5,3.5) * RNG.random()  # mean of distribution
+        sigma = RNG.integers(1,2.5) * RNG.random()  # standard deviation of distribution
+        s = RNG.lognormal(mean=mu, sigma=sigma, size=RNG.integers(50,2000))
+        num_bins = RNG.integers(15,100)
+
+        # randomize color of bins
+        for i in range(3):
+            color = round(RNG.random(), 1)
+            rgb_bin.append(color)
+
+        # add alpha 0 to hide bins 50% of the time (only function line is visible)
+        if RNG.random() > 0.5:
+            rgb_bin.append(0)
+
+        # the histogram of the data
+        count, bins, ignored = plt.hist(s, num_bins, density=True, color=rgb_bin)
+
+        # add a 'best fit' line
+        x = np.linspace(min(bins), max(bins), 1000)
+        y = np.exp(-(np.log(x) - mu)**2 / (2 * sigma**2)) / (x * sigma * np.sqrt(2 * np.pi))
+
+        # color of line
+        for i in range(3):
+            color = round(RNG.random(), 1)
+            rgb_line.append(color)
+
+        # solid or dashed line
+        if RNG.random() > 0.5:
+            line_style = '-'
+        else:
+            line_style = '--'
+
+        ax.plot(x, y, line_style, color=rgb_line, linewidth=RNG.random()*3)
+
+        # fig face color
+        for i in range(3):
+            color = round(RNG.random(), 1)
+            rgb_face.append(color)
+        if rgb_face == RGB_BLACK:
+            rgb_face == RGB_WHITE
+        if RNG.random() > 0.6: # create more white graphs
+            rgb_face = RGB_WHITE
+
+        fig.set_facecolor(rgb_face)
+
+        # ax face color
+        for i in range(3):
+            color = round(RNG.random(), 1)
+            rgb_ax.append(color)
+        if RNG.random() > 0.6:
+            rgb_ax = RGB_WHITE
+
+        ax.set_facecolor(rgb_ax)
+        
+        # x label rgb
+        for i in range(3):
+            color = round(RNG.random(), 1)
+            rgb_xlabel.append(color)
+        # y label rgb
+        for i in range(3):
+            color = round(RNG.random(), 1)
+            rgb_ylabel.append(color)
+        # title rgb
+        for i in range(3):
+            color = round(RNG.random(), 1)
+            rgb_title.append(color)
+
+        # set lorum ipsum labels
+        ax.set_ylabel(' '.join(np.random.choice(lorumipsum, size = RNG.integers(2,5), replace=False)), color=rgb_ylabel, fontsize=max(10, RNG.integers(20,30)*RNG.random()))
+        ax.set_xlabel(' '.join(np.random.choice(lorumipsum, size = RNG.integers(2,5), replace=False)), color=rgb_xlabel, fontsize=max(10, RNG.integers(20,30)*RNG.random()))
+        ax.set_title(' '.join(np.random.choice(lorumipsum, size = RNG.integers(2,5), replace=False)), color=rgb_title, fontsize=max(10, RNG.integers(20,30)*RNG.random()))
+
+        # add grid and adjust x limit 50% of the time
+        if RNG.random() > 0.5:
+            plt.grid(color='k', linestyle='-', linewidth=RNG.random())
+
+        # adjust x limit 50% of the time
+        if RNG.random() > 0.5:
+            plt.xlim(0)
+
+        # Tweak spacing to prevent clipping of ylabel
+        fig.tight_layout()
+
+        # save graph to folder path
+        fig.savefig(os.path.join(save_path, f'norm_{graph}.{save_format}'), dpi=save_dpi)
+        plt.close(fig) # save memory usage
+
+    end_time = time.time()
+    return f'Runtime: {round(end_time - start_time, 3)} seconds'
 
 # %%
