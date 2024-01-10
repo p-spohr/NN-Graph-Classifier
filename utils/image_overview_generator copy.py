f2821417f9fde015089d958012e053863069ef3b
Patrick <spohrpatrick@gmail.com>
2024-01-10 13:00:27 +0100

    evaluate generated graphs with CIFAR_SCP_1 model

--- utils/image_overview_generator copy.py
+++ utils/image_overview_generator copy.py
@@ -23,32 +23,28 @@ for dirpath, dirnames, filenames in os.walk(work_path):
 
 ##### show 3 x 3 image of graphs #####
 
-
-GRAPH_DIST = ['exp', 'lognorm', 'norm', 'unif']
-GRAPH_DIM = ['32x32','115x86','153x115','460x345']
-INDEX_DIST = 3
-INDEX_DIM = 0
-
 # work_path = f'c:\\Users\\pat_h\\htw_berlin_datasets\\dist_datasets\\{GRAPH_DIST[INDEX]}'
-work_path = f'c:\\Users\\pat_h\\htw_berlin_datasets\\dist_datasets\\classifier_dataset_{GRAPH_DIM[INDEX_DIM]}\\{GRAPH_DIST[INDEX_DIST]}_{GRAPH_DIM[INDEX_DIM]}'
+# work_path = f'c:\\Users\\pat_h\\htw_berlin_datasets\\dist_datasets\\classifier_dataset_{GRAPH_DIM[INDEX_DIM]}\\{GRAPH_DIST[INDEX_DIST]}_{GRAPH_DIM[INDEX_DIM]}'
+
+work_path = f'c:\\Users\\pat_h\\htw_berlin_datasets\\0Archive\\graph_dataset\\raw_data'
 
 plt.figure(figsize=(10,8))
 count = 1
 for dirpath, dirnames, filenames in os.walk(work_path):
     while count < 10:
-        for file in filenames[0:9]:
+        for file in filenames[1000:1009]:
             plt.subplot(3, 3, count)
             im = Image.open(os.path.join(work_path, file))
             plt.imshow(im)
             plt.axis('off')
-            plt.title(str(file))
+            # plt.title(str(file))
             count += 1
 
 
-file_name = f"{GRAPH_DIST[INDEX_DIST]}_{GRAPH_DIM[INDEX_DIM]}_overview"
+file_name = f"scraped_overview_original"
 save_path = f"C:\\Users\\pat_h\\OneDrive\\Desktop\\public-repos\\NN-Graph-Classifier\\presentation\\images\\overviews\\{file_name}"
 
-plt.suptitle(f'{GRAPH_DIST[INDEX_DIST].capitalize()} Distributions {GRAPH_DIM[INDEX_DIST]}', fontsize=15.0, y=0.95)
+plt.suptitle(f'Scraped Graphs Original', fontsize=15.0, y=0.95)
 
 plt.savefig(save_path)
 
@@ -91,7 +87,7 @@ for graph_type in GRAPH_DIST:
 ##### get overview for scraped graphs and natural images #####
 
 FOLDER_NAMES = ['generated', 'natural', 'scraped']
-INDEX = 0
+INDEX = 2
 
 work_path = f'C:\\Users\\pat_h\\htw_berlin_datasets\\CIFAR_GEN_SCP_1_DATASET\\{FOLDER_NAMES[INDEX]}'
 
